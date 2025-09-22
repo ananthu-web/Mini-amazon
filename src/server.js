5612e5e89c66a015d24e33222ee4e66b8c459840
ananthu-web <myelectonicmailjr11@gmail.com>
2025-09-22 19:36:22 +0530

    k

--- src/server.js
+++ src/server.js
@@ -19,12 +19,16 @@ app.use(express.json()); // ✅ handle JSON too
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.set('view engine', 'ejs');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log("✅ MongoDB connected"))
-.catch(err => console.error("❌ MongoDB connection error:", err));
+
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+// .then(() => console.log("✅ MongoDB connected"))
+// .catch(err => console.error("❌ MongoDB connection error:", err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch(err => console.error("❌ MongoDB connection error:", err));
 
 
 
